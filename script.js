44bf27a0a09f5728fad328205c57ba1a5383c028
Harry Williams <hcw29@kent.ac.uk>
2023-12-14 16:13:32 +0000

    Finished Product Pt.1

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Need to update with display prop.
+
 function navMenuOpen(){
     let hiddenMenu = document.getElementById('hiddenMenu');
     let displayStyle = window.getComputedStyle(hiddenMenu).display;
@@ -11,15 +11,15 @@ function navMenuOpen(){
 
 function funTime(){
     let sliderNum = document.getElementById('slider').value;
-    if(sliderNum <= 10){
+    if(sliderNum < 10){
         document.body.style.backgroundColor = "rgb(3, 5, 48)";
-    } else if (sliderNum <= 20){
+    } else if (sliderNum < 20){
         document.body.style.backgroundColor = "red";
-    } else if (sliderNum <= 30){
+    } else if (sliderNum < 30){
         document.body.style.backgroundColor = "green";
-    } else if (sliderNum <= 40){
+    } else if (sliderNum < 40){
         document.body.style.backgroundColor = "orange";
-    } else if (sliderNum <= 50){
+    } else if (sliderNum < 50){
         document.body.style.backgroundColor = "cyan";
     }
 }
